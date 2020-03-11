9641cda78a95e060cdd76b18b0a3ab9e69af9b1d
jjzcru <jjzcru@gmail.com>
2020-03-11 14:44:33 -0500

    Set default behavior for root command

--- internal/cli/command/commands/commands.go
+++ internal/cli/command/commands/commands.go
@@ -8,8 +8,8 @@ import (
 	"github.com/jjzcru/elk/internal/cli/command/ls"
 	"github.com/jjzcru/elk/internal/cli/command/run"
 	"github.com/jjzcru/elk/internal/cli/command/version"
-
 	"github.com/spf13/cobra"
+	"os"
 )
 
 // Execute starts the CLI application
@@ -17,6 +17,12 @@ func Execute() error {
 	var rootCmd = &cobra.Command{
 		Use:   "elk",
 		Short: "Minimalist yaml based task runner 🦌",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				_ = cmd.Help()
+				os.Exit(0)
+			}
+		},
 	}
 	rootCmd.AddCommand(
 		version.NewVersionCommand(),
