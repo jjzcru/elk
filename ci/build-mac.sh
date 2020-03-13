0278f51e6617589463e619babcb39458e57216f4
Jose J. Cruz Torres <jjzcru@gmail.com>
2020-03-13 18:07:45 -0500

    Fix build on mac

--- ci/build-mac.sh
+++ ci/build-mac.sh
@@ -5,22 +5,32 @@ BASE_PATH=$(pwd)
 BUILD_PATH=$(pwd)/bin
 MODULE_PATH=$(pwd)/cmd/elk
 
-declare -A platforms
-platforms[darwin,0]=386
-platforms[darwin,1]=amd64
+GOOS=darwin
 
-for key in "${!platforms[@]}"; do
-    GOOS=${key::-2}
-    GOARCH=${platforms[$key]}
-    cd $MODULE_PATH
-    NAME=elk
+# Build for 386
+GOARCH=386
+cd $MODULE_PATH
+NAME=elk
 
-    BIN_PATH=$BUILD_PATH/$NAME
-    go build -o $BIN_PATH
+BIN_PATH=$BUILD_PATH/$NAME
+go build -o $BIN_PATH
 
-    cd $BUILD_PATH
-    ZIP_PATH=${BIN_PATH}_v${VERSION}_${GOOS}_${GOARCH}.zip
+cd $BUILD_PATH
+ZIP_PATH=${BIN_PATH}_v${VERSION}_${GOOS}_${GOARCH}.zip
 
-    zip $ZIP_PATH $NAME
-    rm $NAME
-done
+zip $ZIP_PATH $NAME
+rm $NAME
+
+# Build for amd64
+GOARCH=amd64
+cd $MODULE_PATH
+NAME=elk
+
+BIN_PATH=$BUILD_PATH/$NAME
+go build -o $BIN_PATH
+
+cd $BUILD_PATH
+ZIP_PATH=${BIN_PATH}_v${VERSION}_${GOOS}_${GOARCH}.zip
+
+zip $ZIP_PATH $NAME
+rm $NAME
