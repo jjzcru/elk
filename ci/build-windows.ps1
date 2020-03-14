ad0c1b376f99ff1e25c9a03105eb7d0f2c6a8023
jjzcru <jjzcru@gmail.com>
2020-03-13 19:44:01 -0500

    Test Powershell

--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -19,7 +19,7 @@ go build -o "$BIN_PATH.exe"
 
 cd "$BUILD_PATH"
 $ZIP_PATH = "$($BIN_PATH)_v$($VERSION)_$($GOOS)_$($GOARCH).zip"
-compress-archive "$NAME.exe" $ZIP_PATH
+compress-archive "$($NAME).exe" $ZIP_PATH
 # rm ${NAME}.exe
 
 # amd64
