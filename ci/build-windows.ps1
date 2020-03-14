93695ff2f3840e4cdd5afb0d52977654ea7c208f
jjzcru <jjzcru@gmail.com>
2020-03-13 19:13:05 -0500

    Create powershell script for build

---
+++ ci/build-windows.ps1
@@ -0,0 +1,36 @@
+$VERSION = Get-Content .\VERSION -Raw
+$BASE_PATH = (Get-Item -Path ".\").FullName
+$BUILD_PATH = "$((Get-Item -Path ".\").FullName)\bin"
+$MODULE_PATH = "$((Get-Item -Path ".\").FullName)\cmd\elk"
+
+$env:GOOS = "windows"
+$GOOS = "windows"
+
+$NAME = "elk"
+
+# 386
+$env:GOARCH = "386"
+$GOARCH = "386"
+cd $MODULE_PATH
+
+$BIN_PATH = "$($BUILD_PATH)/$($NAME))"
+go build -o $BIN_PATH.exe
+
+cd $BUILD_PATH
+$ZIP_PATH = "$BIN_PATH_v$VERSION_$GOOS_$GOARCH.zip"
+compress-archive "$NAME.exe" $ZIP_PATH
+rm ${NAME}.exe
+
+# amd64
+$env:GOARCH = "amd64"
+$GOARCH = "amd64"
+cd $MODULE_PATH
+
+$BIN_PATH = "$($BUILD_PATH)/$($NAME))"
+go build -o $BIN_PATH.exe
+
+cd $BUILD_PATH
+$ZIP_PATH = "$BIN_PATH_v$VERSION_$GOOS_$GOARCH.zip"
+compress-archive "$NAME.exe" $ZIP_PATH
+rm ${NAME}.exe
+
