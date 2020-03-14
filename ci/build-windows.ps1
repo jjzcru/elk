ca94bfbc716b70955e77993696e2c68aa3a6ba77
jjzcru <jjzcru@gmail.com>
2020-03-13 19:26:39 -0500

    Test Powershell

--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -13,24 +13,25 @@ $env:GOARCH = "386"
 $GOARCH = "386"
 cd $MODULE_PATH
 
-$BIN_PATH = "$($BUILD_PATH)/$($NAME))"
+$BIN_PATH = "$BUILD_PATH\$NAME"
+echo $BIN_PATH
 go build -o $BIN_PATH.exe
 
-cd $BUILD_PATH
-$ZIP_PATH = "$BIN_PATH_v$VERSION_$GOOS_$GOARCH.zip"
-compress-archive "$NAME.exe" $ZIP_PATH
-rm ${NAME}.exe
+#cd $BUILD_PATH
+#$ZIP_PATH = "$BIN_PATH_v$VERSION_$GOOS_$GOARCH.zip"
+#compress-archive "$NAME.exe" $ZIP_PATH
+#rm ${NAME}.exe
 
 # amd64
-$env:GOARCH = "amd64"
-$GOARCH = "amd64"
-cd $MODULE_PATH
+#$env:GOARCH = "amd64"
+#$GOARCH = "amd64"
+#cd $MODULE_PATH
 
-$BIN_PATH = "$($BUILD_PATH)/$($NAME))"
-go build -o $BIN_PATH.exe
+#$BIN_PATH = "$($BUILD_PATH)/$($NAME))"
+#go build -o $BIN_PATH.exe
 
-cd $BUILD_PATH
-$ZIP_PATH = "$BIN_PATH_v$VERSION_$GOOS_$GOARCH.zip"
-compress-archive "$NAME.exe" $ZIP_PATH
-rm ${NAME}.exe
+#cd $BUILD_PATH
+#$ZIP_PATH = "$BIN_PATH_v$VERSION_$GOOS_$GOARCH.zip"
+#compress-archive "$NAME.exe" $ZIP_PATH
+#rm ${NAME}.exe
 
