fcfab5ff44523e8cce639bd54f45de1ed21b6010
jjzcru <jjzcru@gmail.com>
2020-03-22 02:29:23 -0500

    Update windows build

--- ci/build-windows.ps1
+++ ci/build-windows.ps1
@@ -20,7 +20,7 @@ $BIN_PATH = "$BUILD_PATH\$NAME"
 echo "ARCH: $($GOARCH)"
 echo "--------------------------"
 echo "Building $($GOARCH) binary"
-go build -ldflags "-X main.v=$VERSION -X main.o=$GOOS -X main.arch=$GOARCH -X main.commit=$COMMIT" -o "$BIN_PATH.exe"
+go build -o "$BIN_PATH.exe"
 echo "Build successful"
 
 cd "$BUILD_PATH"
@@ -32,24 +32,3 @@ rm "$NAME.exe"
 echo "Compress successful"
 echo "--------------------------"
 echo ""
-
-# amd64
-$env:GOARCH = "amd64"
-$GOARCH = "amd64"
-cd $MODULE_PATH
-
-$BIN_PATH = "$BUILD_PATH\$NAME"
-echo "ARCH: $($GOARCH)"
-echo "--------------------------"
-echo "Building $($GOARCH) binary"
-go build -ldflags "-X main.v=$VERSION -X main.o=$GOOS -X main.arch=$GOARCH -X main.commit=$COMMIT" -o "$BIN_PATH.exe"
-echo "Build successful"
-
-cd "$BUILD_PATH"
-$ZIP_PATH = "$($BIN_PATH)_$($VERSION)_$($GOOS)_$($GOARCH).zip"
-
-echo "Compressing $($GOARCH) binary"
-compress-archive "$BIN_PATH.exe" "$ZIP_PATH" -Force
-rm "$NAME.exe"
-echo "Compress successful"
-echo "--------------------------"
