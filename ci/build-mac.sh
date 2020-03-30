e42dd61abc3e070b4cf9714a00587da6cdcef0c0
Jose J. Cruz Torres <jjzcru@gmail.com>
2020-03-30 09:48:48 -0500

    Add mac build script

--- ci/build-mac.sh
+++ ci/build-mac.sh
@@ -11,10 +11,39 @@ fill_date=$(date +'%d_%T_%Y')
 
 DATE="${day}_${month}_${fill_date}"
 
+if [ -z "$VERSION" ]
+then
+    VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
+fi
+
+if [ -z "$COMMIT" ]
+then
+    COMMIT=$(git rev-parse --short HEAD)
+fi
+
+if [ -z "$GOVERSION" ]
+then
+    GOVERSION=$(go version | awk '{print $3}')
+fi
+
 echo "BUILT"
 echo "VERSION: $VERSION"
 echo "COMMIT: $COMMIT"
 echo "DATE: $DATE"
+
+# Build for amd64
+GOARCH=amd64
+cd $MODULE_PATH
+NAME=elk
+
+BIN_PATH=$BUILD_PATH/$NAME
+go build -ldflags "-X main.v=$VERSION -X main.o=$GOOS -X main.arch=$GOARCH -X main.commit=$COMMIT -X main.date=$DATE -X main.goVersion=$GOVERSION" -o $BIN_PATH
+
+cd $BUILD_PATH
+ZIP_PATH=${BIN_PATH}_${VERSION}_${GOOS}_${GOARCH}.zip
+
+zip $ZIP_PATH $NAME
+rm $NAME
 
 # Build for 386
 GOARCH=386
@@ -28,20 +57,4 @@ cd $BUILD_PATH
 ZIP_PATH=${BIN_PATH}_${VERSION}_${GOOS}_${GOARCH}.zip
 
 zip $ZIP_PATH $NAME
-sleep 5s
-rm $NAME
-
-# Build for amd64
-GOARCH=amd64
-cd $MODULE_PATH
-NAME=elk
-
-BIN_PATH=$BUILD_PATH/$NAME
-go build -o $BIN_PATH
-
-cd $BUILD_PATH
-ZIP_PATH=${BIN_PATH}_${VERSION}_${GOOS}_${GOARCH}.zip
-
-zip $ZIP_PATH $NAME
-sleep 5s
 rm $NAME
